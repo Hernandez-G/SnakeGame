85802fcf5f807f99ed26ba11fc4905a7f1fcd00c
hernandez-g <g5hernandez@yahoo.com>
2022-07-12 19:02:49 -0700

    moving food

--- main.js
+++ main.js
@@ -6,16 +6,42 @@ const cols = 20;
 let gameBoard;
 let context;
 
+//snake head
+
+const snakeX = blockSize * 5;
+const snakeY = blockSize * 5;
+
+//snake food
+
+let foodX;
+let foodY;
+
+
 window.onload = function() {
     gameBoard = document.getElementById("gameBoard");
     gameBoard.height = rows * blockSize;
     gameBoard.width = cols * blockSize;
-    context = board.getContext("2d");
+    context = gameBoard.getContext("2d");
+    
+    placeFood();
 
     update();
 }
 
 function update() {
     context.fillStyle="black";
-    
+    context.fillRect(0, 0, gameBoard.width, gameBoard.height);
+
+    context.fillStyle="lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle="red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+
+function placeFood() {
+    //
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
